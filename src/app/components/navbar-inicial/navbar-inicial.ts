dd2433d4b7833181fd37662c551da002cc811ad8
piragna <gonzalovarelaalagna@gmail.com>
2026-05-20 17:00:19 -0300

    Uso de routerlink para navegar hacia la pagina de login

--- src/app/components/navbar-inicial/navbar-inicial.ts
+++ src/app/components/navbar-inicial/navbar-inicial.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar-inicial',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navbar-inicial.html',
   styleUrl: './navbar-inicial.css',
 })
-export class NavbarInicial {}
+export class NavbarInicial { }
--- src/app/components/navbar-login/navbar-login.ts
+++ src/app/components/navbar-login/navbar-login.ts
@@ -6,4 +6,4 @@ import { Component } from '@angular/core';
   templateUrl: './navbar-login.html',
   styleUrl: './navbar-login.css',
 })
-export class NavbarLogin {}
+export class NavbarLogin { }
