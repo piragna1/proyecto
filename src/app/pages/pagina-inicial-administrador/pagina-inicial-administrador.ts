2b2363595a60b0c456981d53bfa5e88eb172fbfe
piragna <gonzalovarelaalagna@gmail.com>
2026-05-21 10:52:02 -0300

    .ts pagina-inicial-administrador

--- src/app/pages/pagina-inicial-administrador/pagina-inicial-administrador.ts
+++ src/app/pages/pagina-inicial-administrador/pagina-inicial-administrador.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavbarInicialAdmin } from '../../components/navbar-inicial-admin/navbar-inicial-admin';
+import { Footer } from "../../shared/components/footer/footer";
 
 @Component({
   selector: 'app-pagina-inicial-administrador',
-  imports: [],
+  imports: [NavbarInicialAdmin, Footer],
   templateUrl: './pagina-inicial-administrador.html',
   styleUrl: './pagina-inicial-administrador.css',
 })
-export class PaginaInicialAdministrador {}
+export class PaginaInicialAdministrador { }
