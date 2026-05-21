5228513baf90908964e220463a160356ba775592
piragna <gonzalovarelaalagna@gmail.com>
2026-05-21 12:47:35 -0300

    Estructura del componente de alta de peluqueros

--- src/app/components/componente-alta-peluquero/componente-alta-peluquero.ts
+++ src/app/components/componente-alta-peluquero/componente-alta-peluquero.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-componente-alta-peluquero',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './componente-alta-peluquero.html',
   styleUrl: './componente-alta-peluquero.css',
 })
-export class ComponenteAltaPeluquero {}
+export class ComponenteAltaPeluquero { }
