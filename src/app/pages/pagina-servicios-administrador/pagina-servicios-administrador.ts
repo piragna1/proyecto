c2d9f0e8e5d37878903c3193327a2861fa183140
piragna <gonzalovarelaalagna@gmail.com>
2026-05-21 12:06:56 -0300

    Estructura de la pagina de servicios del administrador.

--- src/app/pages/pagina-servicios-administrador/pagina-servicios-administrador.ts
+++ src/app/pages/pagina-servicios-administrador/pagina-servicios-administrador.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarInicialAdmin } from '../../components/navbar-inicial-admin/navbar-inicial-admin';
+import { ComponenteServiciosAdministrador } from '../../components/componente-servicios-administrador/componente-servicios-administrador';
+import { Footer } from '../../shared/components/footer/footer';
 
 @Component({
   selector: 'app-pagina-servicios-administrador',
-  imports: [],
+  imports: [NavbarInicialAdmin, ComponenteServiciosAdministrador, Footer],
   templateUrl: './pagina-servicios-administrador.html',
   styleUrl: './pagina-servicios-administrador.css',
 })
-export class PaginaServiciosAdministrador {}
+export class PaginaServiciosAdministrador { }
