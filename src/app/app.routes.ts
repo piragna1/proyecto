7acd25ecbf0014a86faf2233ee331133e70ab7bc
piragna <gonzalovarelaalagna@gmail.com>
2026-05-21 10:04:35 -0300

    Enrutamiento hacia pagina-gome

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { PaginaInicial } from './pages/pagina-inicial/pagina-inicial';
 import { PaginaLogin } from './pages/pagina-login/pagina-login';
 import { PaginaRegistro } from './pages/pagina-registro/pagina-registro';
 import { PaginaAdmin } from './pages/pagina-admin/pagina-admin';
+import { PaginaHome } from './pages/pagina-home/pagina-home';
 
 export const routes: Routes = [
     {
@@ -16,6 +17,9 @@ export const routes: Routes = [
     },
     {
         path: 'admin', component: PaginaAdmin
+    },
+    {
+        path: 'home', component: PaginaHome
     },
     {
         path: '**', redirectTo: ''
