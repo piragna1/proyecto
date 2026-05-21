26e38ed4da3c771696e250e06553efd4ce64fff0
piragna <gonzalovarelaalagna@gmail.com>
2026-05-21 20:22:40 -0300

    Estructura de la pagina de clientes del peluquero.

--- src/app/pages/pagina-clientes-peluquero/pagina-clientes-peluquero.ts
+++ src/app/pages/pagina-clientes-peluquero/pagina-clientes-peluquero.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarInicialPeluquero } from '../../components/navbar-inicial-peluquero/navbar-inicial-peluquero';
+import { ComponenteClientesPeluquero } from '../../components/componente-clientes-peluquero/componente-clientes-peluquero';
+import { Footer } from '../../shared/components/footer/footer';
 
 @Component({
   selector: 'app-pagina-clientes-peluquero',
-  imports: [],
+  imports: [NavbarInicialPeluquero, ComponenteClientesPeluquero, Footer],
   templateUrl: './pagina-clientes-peluquero.html',
   styleUrl: './pagina-clientes-peluquero.css',
 })
-export class PaginaClientesPeluquero {}
+export class PaginaClientesPeluquero { }
