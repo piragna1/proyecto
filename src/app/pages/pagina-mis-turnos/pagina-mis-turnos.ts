4575f0c6ff33bde354c027de7d2b454482cbe8a2
piragna <gonzalovarelaalagna@gmail.com>
2026-05-21 10:37:37 -0300

    Importación de componentes hacia el componente de la pagina pagina-mis-turnos del usuario.

--- src/app/pages/pagina-mis-turnos/pagina-mis-turnos.ts
+++ src/app/pages/pagina-mis-turnos/pagina-mis-turnos.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarUsuarioMisTurnos } from '../../components/navbar-usuario-mis-turnos/navbar-usuario-mis-turnos';
+import { ComponenteMisTurnos } from '../../components/componente-mis-turnos/componente-mis-turnos';
+import { Footer } from "../../shared/components/footer/footer";
 
 @Component({
   selector: 'app-pagina-mis-turnos',
-  imports: [],
+  imports: [NavbarUsuarioMisTurnos, ComponenteMisTurnos, Footer],
   templateUrl: './pagina-mis-turnos.html',
   styleUrl: './pagina-mis-turnos.css',
 })
-export class PaginaMisTurnos {}
+export class PaginaMisTurnos { }
