19bd75470fa9fc93fc943df96abfab460a9cae95
piragna <gonzalovarelaalagna@gmail.com>
2026-05-21 20:38:24 -0300

    Estructura inicial de la página de turnos del peluquero.

--- src/app/pages/pagina-turnos-peluquero/pagina-turnos-peluquero.ts
+++ src/app/pages/pagina-turnos-peluquero/pagina-turnos-peluquero.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarInicialPeluquero } from '../../components/navbar-inicial-peluquero/navbar-inicial-peluquero';
+import { ComponenteTurnosPeluquero } from '../../components/componente-turnos-peluquero/componente-turnos-peluquero';
+import { Footer } from '../../shared/components/footer/footer';
 
 @Component({
   selector: 'app-pagina-turnos-peluquero',
-  imports: [],
+  imports: [NavbarInicialPeluquero, ComponenteTurnosPeluquero, Footer],
   templateUrl: './pagina-turnos-peluquero.html',
   styleUrl: './pagina-turnos-peluquero.css',
 })
-export class PaginaTurnosPeluquero {}
+export class PaginaTurnosPeluquero { }
