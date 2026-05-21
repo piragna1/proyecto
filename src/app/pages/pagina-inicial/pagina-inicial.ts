f2711e03c6a78927ba9a58384239e8e9cd323f4d
piragna <gonzalovarelaalagna@gmail.com>
2026-05-21 09:53:26 -0300

    Agregamos componente footer a la pagina inicial

--- src/app/pages/pagina-inicial/pagina-inicial.ts
+++ src/app/pages/pagina-inicial/pagina-inicial.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavbarInicial } from '../../components/navbar-inicial/navbar-inicial';
+import { Footer } from "../../shared/components/footer/footer";
 
 @Component({
   selector: 'app-pagina-inicial',
-  imports: [NavbarInicial],
+  imports: [NavbarInicial, Footer],
   templateUrl: './pagina-inicial.html',
   styleUrl: './pagina-inicial.css',
 })
