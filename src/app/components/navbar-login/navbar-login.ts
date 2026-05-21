7593500a5bf2e019cb91bcf74f8283d7a2310f15
piragna <gonzalovarelaalagna@gmail.com>
2026-05-21 09:51:27 -0300

    Agregado RouterLink a imports en el .ts del navbar de la pagina de login

--- src/app/components/navbar-login/navbar-login.ts
+++ src/app/components/navbar-login/navbar-login.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-navbar-login',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navbar-login.html',
   styleUrl: './navbar-login.css',
 })
