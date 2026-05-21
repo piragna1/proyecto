f315c4b85e0ebcc3e3bef385ea14d5643d61232b
piragna <gonzalovarelaalagna@gmail.com>
2026-05-21 09:53:48 -0300

    Agregamos a la pagina de login el formulario y el footer

--- src/app/pages/pagina-login/pagina-login.ts
+++ src/app/pages/pagina-login/pagina-login.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarLogin } from '../../components/navbar-login/navbar-login';
+import { FormularioLogin } from '../../components/formulario-login/formulario-login';
+import { Footer } from "../../shared/components/footer/footer";
 
 @Component({
   selector: 'app-pagina-login',
-  imports: [NavbarLogin],
+  imports: [NavbarLogin, FormularioLogin, Footer],
   templateUrl: './pagina-login.html',
   styleUrl: './pagina-login.css',
 })
