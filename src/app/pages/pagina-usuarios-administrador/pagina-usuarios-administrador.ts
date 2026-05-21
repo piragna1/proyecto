9a3ae5796a067bf955b45bb6c496200637858583
piragna <gonzalovarelaalagna@gmail.com>
2026-05-21 10:58:18 -0300

    Estructura de la pagina pagina-usuarios-administrador

--- src/app/pages/pagina-usuarios-administrador/pagina-usuarios-administrador.ts
+++ src/app/pages/pagina-usuarios-administrador/pagina-usuarios-administrador.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarUsuariosAdministrador } from '../../components/navbar-usuarios-administrador/navbar-usuarios-administrador';
+import { Footer } from '../../shared/components/footer/footer';
+import { ComponenteUsuariosAdministrador } from '../../components/componente-usuarios-administrador/componente-usuarios-administrador';
 
 @Component({
   selector: 'app-pagina-usuarios-administrador',
-  imports: [],
+  imports: [NavbarUsuariosAdministrador, ComponenteUsuariosAdministrador, Footer],
   templateUrl: './pagina-usuarios-administrador.html',
   styleUrl: './pagina-usuarios-administrador.css',
 })
-export class PaginaUsuariosAdministrador {}
+export class PaginaUsuariosAdministrador { }
