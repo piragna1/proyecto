669d3ebd042424381ba0a4379f70683d30ba86da
piragna <gonzalovarelaalagna@gmail.com>
2026-05-21 11:37:31 -0300

    Estructura de la pagina de peluqueros del administrador

--- src/app/pages/pagina-peluqueros-administrador/pagina-peluqueros-administrador.ts
+++ src/app/pages/pagina-peluqueros-administrador/pagina-peluqueros-administrador.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarInicialAdmin } from '../../components/navbar-inicial-admin/navbar-inicial-admin';
+import { ComponentePeluquerosAdministrador } from '../../components/componente-peluqueros-administrador/componente-peluqueros-administrador';
+import { Footer } from '../../shared/components/footer/footer';
 
 @Component({
   selector: 'app-pagina-peluqueros-administrador',
-  imports: [],
+  imports: [NavbarInicialAdmin, ComponentePeluquerosAdministrador, Footer],
   templateUrl: './pagina-peluqueros-administrador.html',
   styleUrl: './pagina-peluqueros-administrador.css',
 })
-export class PaginaPeluquerosAdministrador {}
+export class PaginaPeluquerosAdministrador { }
