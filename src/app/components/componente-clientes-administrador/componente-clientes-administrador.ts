e2eeeacad24244f7d2030a6dce00c49d9b50c747
piragna <gonzalovarelaalagna@gmail.com>
2026-05-21 11:20:21 -0300

    Importación de RouterLink hacia el componente de clientes del administrador

--- src/app/components/componente-clientes-administrador/componente-clientes-administrador.ts
+++ src/app/components/componente-clientes-administrador/componente-clientes-administrador.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-componente-clientes-administrador',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './componente-clientes-administrador.html',
   styleUrl: './componente-clientes-administrador.css',
 })
-export class ComponenteClientesAdministrador {}
+export class ComponenteClientesAdministrador { }
