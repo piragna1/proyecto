913854ee15fd2fe6b321e392b8305ed60d0cac58
piragna <gonzalovarelaalagna@gmail.com>
2026-05-21 13:12:22 -0300

    Estructura de la pagina de alta de servicios de los administradores

--- src/app/pages/pagina-alta-servicio/pagina-alta-servicio.ts
+++ src/app/pages/pagina-alta-servicio/pagina-alta-servicio.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarInicialAdmin } from '../../components/navbar-inicial-admin/navbar-inicial-admin';
+import { ComponenteAltaServicio } from '../../components/componente-alta-servicio/componente-alta-servicio';
+import { Footer } from '../../shared/components/footer/footer';
 
 @Component({
   selector: 'app-pagina-alta-servicio',
-  imports: [],
+  imports: [NavbarInicialAdmin, ComponenteAltaServicio, Footer],
   templateUrl: './pagina-alta-servicio.html',
   styleUrl: './pagina-alta-servicio.css',
 })
-export class PaginaAltaServicio {}
+export class PaginaAltaServicio { }
