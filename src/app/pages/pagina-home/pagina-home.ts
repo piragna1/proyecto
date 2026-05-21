e56bf31dee73fdcee84f377d3a1e76d2faa60fb1
piragna <gonzalovarelaalagna@gmail.com>
2026-05-21 10:04:55 -0300

    Agregamos componentes navbar componenteHome y footer a pagina home

--- src/app/pages/pagina-home/pagina-home.ts
+++ src/app/pages/pagina-home/pagina-home.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarUsuarioHome } from '../../components/navbar-usuario-home/navbar-usuario-home';
+import { ComponenteHome } from "../../components/componente-home/componente-home";
+import { Footer } from "../../shared/components/footer/footer";
 
 @Component({
   selector: 'app-pagina-home',
-  imports: [],
+  imports: [NavbarUsuarioHome, ComponenteHome, Footer],
   templateUrl: './pagina-home.html',
   styleUrl: './pagina-home.css',
 })
-export class PaginaHome {}
+export class PaginaHome { }
