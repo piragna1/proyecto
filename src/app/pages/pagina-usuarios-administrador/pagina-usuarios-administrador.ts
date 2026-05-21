24cb4b448010f234eb961936499b023b4076953b
piragna <gonzalovarelaalagna@gmail.com>
2026-05-21 11:55:02 -0300

    Utilizacion del navbar inicial en la pagina de usuarios del administrador

--- src/app/pages/pagina-usuarios-administrador/pagina-usuarios-administrador.ts
+++ src/app/pages/pagina-usuarios-administrador/pagina-usuarios-administrador.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { NavbarUsuariosAdministrador } from '../../components/navbar-usuarios-administrador/navbar-usuarios-administrador';
 import { Footer } from '../../shared/components/footer/footer';
 import { ComponenteUsuariosAdministrador } from '../../components/componente-usuarios-administrador/componente-usuarios-administrador';
+import { NavbarInicialAdmin } from "../../components/navbar-inicial-admin/navbar-inicial-admin";
 
 @Component({
   selector: 'app-pagina-usuarios-administrador',
-  imports: [NavbarUsuariosAdministrador, ComponenteUsuariosAdministrador, Footer],
+  imports: [NavbarUsuariosAdministrador, ComponenteUsuariosAdministrador, Footer, NavbarInicialAdmin],
   templateUrl: './pagina-usuarios-administrador.html',
   styleUrl: './pagina-usuarios-administrador.css',
 })
