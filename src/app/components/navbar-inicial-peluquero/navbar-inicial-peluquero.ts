2f985a1369a980ce362c4eccccce58522b1226b8
piragna <gonzalovarelaalagna@gmail.com>
2026-05-21 13:24:16 -0300

    Estructura inicial del navbar inicial de las paginas del peluquero.

--- src/app/components/navbar-inicial-peluquero/navbar-inicial-peluquero.ts
+++ src/app/components/navbar-inicial-peluquero/navbar-inicial-peluquero.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-navbar-inicial-peluquero',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navbar-inicial-peluquero.html',
   styleUrl: './navbar-inicial-peluquero.css',
 })
-export class NavbarInicialPeluquero {}
+export class NavbarInicialPeluquero { }
