69e379e9f74869a77337844f0b7f9421227d2feb
piragna <gonzalovarelaalagna@gmail.com>
2026-05-21 10:10:18 -0300

    Enrutamiento a pagina-nuevo-turno

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { PaginaLogin } from './pages/pagina-login/pagina-login';
 import { PaginaRegistro } from './pages/pagina-registro/pagina-registro';
 import { PaginaAdmin } from './pages/pagina-admin/pagina-admin';
 import { PaginaHome } from './pages/pagina-home/pagina-home';
+import { PaginaNuevoTurno } from './pages/pagina-nuevo-turno/pagina-nuevo-turno';
 
 export const routes: Routes = [
     {
@@ -20,6 +21,9 @@ export const routes: Routes = [
     },
     {
         path: 'home', component: PaginaHome
+    },
+    {
+        path: 'nuevo-turno', component: PaginaNuevoTurno
     },
     {
         path: '**', redirectTo: ''
