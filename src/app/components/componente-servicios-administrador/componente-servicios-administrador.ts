1012947c99dbcf129a4a11471ac624898cabf31b
piragna <gonzalovarelaalagna@gmail.com>
2026-05-21 12:02:08 -0300

    Componente de servicios del administrador

---
+++ src/app/components/componente-servicios-administrador/componente-servicios-administrador.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-componente-servicios-administrador',
+  imports: [],
+  templateUrl: './componente-servicios-administrador.html',
+  styleUrl: './componente-servicios-administrador.css',
+})
+export class ComponenteServiciosAdministrador {}
