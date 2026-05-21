dba9b7bbfb79c5895793a7cfcd9bd61046667905
piragna <gonzalovarelaalagna@gmail.com>
2026-05-21 13:23:55 -0300

    Estructura inicial de la pagina inicial del peluquero.

--- src/app/pages/pagina-inicial-peluquero/pagina-inicial-peluquero.ts
+++ src/app/pages/pagina-inicial-peluquero/pagina-inicial-peluquero.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarInicialPeluquero } from '../../components/navbar-inicial-peluquero/navbar-inicial-peluquero';
+import { ComponenteInicialPeluquero } from '../../components/componente-inicial-peluquero/componente-inicial-peluquero';
+import { Footer } from '../../shared/components/footer/footer';
 
 @Component({
   selector: 'app-pagina-inicial-peluquero',
-  imports: [],
+  imports: [NavbarInicialPeluquero, ComponenteInicialPeluquero, Footer],
   templateUrl: './pagina-inicial-peluquero.html',
   styleUrl: './pagina-inicial-peluquero.css',
 })
-export class PaginaInicialPeluquero {}
+export class PaginaInicialPeluquero { }
