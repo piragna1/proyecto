47abd2b98ec583d08f9b1e008fdb110ab9184600
piragna <gonzalovarelaalagna@gmail.com>
2026-05-21 10:14:02 -0300

    Incluyendo componentes en el .ts de la pagina pagina-nuevo-turno

--- src/app/pages/pagina-nuevo-turno/pagina-nuevo-turno.ts
+++ src/app/pages/pagina-nuevo-turno/pagina-nuevo-turno.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarUsuarioNuevoTurno } from '../../components/navbar-usuario-nuevo-turno/navbar-usuario-nuevo-turno';
+import { ComponenteNuevoTurno } from '../../components/componente-nuevo-turno/componente-nuevo-turno';
+import { Footer } from "../../shared/components/footer/footer";
 
 @Component({
   selector: 'app-pagina-nuevo-turno',
-  imports: [],
+  imports: [NavbarUsuarioNuevoTurno, ComponenteNuevoTurno, Footer],
   templateUrl: './pagina-nuevo-turno.html',
   styleUrl: './pagina-nuevo-turno.css',
 })
-export class PaginaNuevoTurno {}
+export class PaginaNuevoTurno { }
