73bd5082f2c2a7aae9487ca4739a1cf7eb6ef29e
piragna <gonzalovarelaalagna@gmail.com>
2026-05-21 20:22:52 -0300

    Estructura del componente de clientes del peluquero.

--- src/app/components/componente-clientes-peluquero/componente-clientes-peluquero.ts
+++ src/app/components/componente-clientes-peluquero/componente-clientes-peluquero.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-componente-clientes-peluquero',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './componente-clientes-peluquero.html',
   styleUrl: './componente-clientes-peluquero.css',
 })
-export class ComponenteClientesPeluquero {}
+export class ComponenteClientesPeluquero { }
