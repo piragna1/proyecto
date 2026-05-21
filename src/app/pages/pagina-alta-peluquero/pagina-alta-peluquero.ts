b903b564b2b1f6b3b6aead0e46c8685057b698d5
piragna <gonzalovarelaalagna@gmail.com>
2026-05-21 12:47:10 -0300

    Estructura de la pagina de alta de peluqueros

--- src/app/pages/pagina-alta-peluquero/pagina-alta-peluquero.ts
+++ src/app/pages/pagina-alta-peluquero/pagina-alta-peluquero.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarInicialAdmin } from '../../components/navbar-inicial-admin/navbar-inicial-admin';
+import { ComponenteAltaPeluquero } from '../../components/componente-alta-peluquero/componente-alta-peluquero';
+import { Footer } from '../../shared/components/footer/footer';
 
 @Component({
   selector: 'app-pagina-alta-peluquero',
-  imports: [],
+  imports: [NavbarInicialAdmin, ComponenteAltaPeluquero, Footer],
   templateUrl: './pagina-alta-peluquero.html',
   styleUrl: './pagina-alta-peluquero.css',
 })
-export class PaginaAltaPeluquero {}
+export class PaginaAltaPeluquero { }
