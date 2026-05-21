2b2fc9286bee100542a85660df162411bf18d5a0
piragna <gonzalovarelaalagna@gmail.com>
2026-05-21 11:55:27 -0300

    Agregación de componente a la pagina inicial del administrador

--- src/app/pages/pagina-inicial-administrador/pagina-inicial-administrador.ts
+++ src/app/pages/pagina-inicial-administrador/pagina-inicial-administrador.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarInicialAdmin } from '../../components/navbar-inicial-admin/navbar-inicial-admin';
 import { Footer } from "../../shared/components/footer/footer";
+import { ComponenteInicialAdministrador } from '../../components/componente-inicial-administrador/componente-inicial-administrador';
 
 @Component({
   selector: 'app-pagina-inicial-administrador',
-  imports: [NavbarInicialAdmin, Footer],
+  imports: [NavbarInicialAdmin, ComponenteInicialAdministrador, Footer],
   templateUrl: './pagina-inicial-administrador.html',
   styleUrl: './pagina-inicial-administrador.css',
 })
