8522411dd900825b058df8ab7430208e797104ce
piragna <gonzalovarelaalagna@gmail.com>
2026-05-21 09:48:05 -0300

    Componente formulario para page registro

---
+++ src/app/components/formulario-registro/formulario-registro.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-formulario-registro',
+  imports: [RouterLink],
+  templateUrl: './formulario-registro.html',
+  styleUrl: './formulario-registro.css',
+})
+export class FormularioRegistro { }
