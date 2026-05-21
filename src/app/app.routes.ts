d0f1596a80401eb8a349fa907937959e84ef609c
piragna <gonzalovarelaalagna@gmail.com>
2026-05-21 09:49:01 -0300

    Enrutamiento de la pagina de reistro y admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { PaginaInicial } from './pages/pagina-inicial/pagina-inicial';
 import { PaginaLogin } from './pages/pagina-login/pagina-login';
+import { PaginaRegistro } from './pages/pagina-registro/pagina-registro';
+import { PaginaAdmin } from './pages/pagina-admin/pagina-admin';
 
 export const routes: Routes = [
     {
@@ -8,6 +10,12 @@ export const routes: Routes = [
     },
     {
         path: 'login', component: PaginaLogin
+    },
+    {
+        path: 'registro', component: PaginaRegistro
+    },
+    {
+        path: 'admin', component: PaginaAdmin
     },
     {
         path: '**', redirectTo: ''
