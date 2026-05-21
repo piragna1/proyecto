cb6a9f18ac7d1355c93171c862f76634cff1558a
piragna <gonzalovarelaalagna@gmail.com>
2026-05-21 13:12:46 -0300

    Estructura del componente de alta de servicios de los administradores

--- src/app/components/componente-alta-servicio/componente-alta-servicio.ts
+++ src/app/components/componente-alta-servicio/componente-alta-servicio.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-componente-alta-servicio',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './componente-alta-servicio.html',
   styleUrl: './componente-alta-servicio.css',
 })
-export class ComponenteAltaServicio {}
+export class ComponenteAltaServicio { }
