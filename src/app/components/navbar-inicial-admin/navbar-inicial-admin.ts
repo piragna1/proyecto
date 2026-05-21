56df6b829aa902c3df85c1ced2d32cce94145eb0
piragna <gonzalovarelaalagna@gmail.com>
2026-05-21 10:51:49 -0300

    Estructura del navbar de la pagina pagina-inicial-administrador

--- src/app/components/navbar-inicial-admin/navbar-inicial-admin.ts
+++ src/app/components/navbar-inicial-admin/navbar-inicial-admin.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-navbar-inicial-admin',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navbar-inicial-admin.html',
   styleUrl: './navbar-inicial-admin.css',
 })
-export class NavbarInicialAdmin {}
+export class NavbarInicialAdmin { }
