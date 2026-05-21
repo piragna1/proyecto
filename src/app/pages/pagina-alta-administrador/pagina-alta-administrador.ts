77bfbd89908c5c087adc85abb3a8685ce8fa4d69
piragna <gonzalovarelaalagna@gmail.com>
2026-05-21 12:47:18 -0300

    Estructura de la pagina de alta de administradores

--- src/app/pages/pagina-alta-administrador/pagina-alta-administrador.ts
+++ src/app/pages/pagina-alta-administrador/pagina-alta-administrador.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarInicialAdmin } from '../../components/navbar-inicial-admin/navbar-inicial-admin';
+import { ComponenteAltaAdministrador } from '../../components/componente-alta-administrador/componente-alta-administrador';
+import { Footer } from '../../shared/components/footer/footer';
 
 @Component({
   selector: 'app-pagina-alta-administrador',
-  imports: [],
+  imports: [NavbarInicialAdmin, ComponenteAltaAdministrador, Footer],
   templateUrl: './pagina-alta-administrador.html',
   styleUrl: './pagina-alta-administrador.css',
 })
-export class PaginaAltaAdministrador {}
+export class PaginaAltaAdministrador { }
