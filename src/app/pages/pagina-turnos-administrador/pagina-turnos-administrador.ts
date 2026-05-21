5904c47f68a4ec87d036483656bc20e7b184b896
piragna <gonzalovarelaalagna@gmail.com>
2026-05-21 12:02:38 -0300

    .ts del componente de la pagina de turnos del administrador.

--- src/app/pages/pagina-turnos-administrador/pagina-turnos-administrador.ts
+++ src/app/pages/pagina-turnos-administrador/pagina-turnos-administrador.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarInicialAdmin } from '../../components/navbar-inicial-admin/navbar-inicial-admin';
+import { ComponenteTurnosAdministrador } from '../../components/componente-turnos-administrador/componente-turnos-administrador';
+import { Footer } from '../../shared/components/footer/footer';
 
 @Component({
   selector: 'app-pagina-turnos-administrador',
-  imports: [],
+  imports: [NavbarInicialAdmin, ComponenteTurnosAdministrador, Footer],
   templateUrl: './pagina-turnos-administrador.html',
   styleUrl: './pagina-turnos-administrador.css',
 })
-export class PaginaTurnosAdministrador {}
+export class PaginaTurnosAdministrador { }
