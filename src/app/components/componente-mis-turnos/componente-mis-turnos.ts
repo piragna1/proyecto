2de6640da7860c0dae8878c0c85b34bd20b1e8d6
piragna <gonzalovarelaalagna@gmail.com>
2026-05-21 21:00:20 -0300

    Nueva estructura del componente mis-turnos del usuario

--- src/app/components/componente-mis-turnos/componente-mis-turnos.ts
+++ src/app/components/componente-mis-turnos/componente-mis-turnos.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-componente-mis-turnos',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './componente-mis-turnos.html',
   styleUrl: './componente-mis-turnos.css',
 })
-export class ComponenteMisTurnos {}
+export class ComponenteMisTurnos { }
