827a75371170e9cacf8c8e808579a14c4fb011bc
piragna <gonzalovarelaalagna@gmail.com>
2026-05-21 21:00:03 -0300

    Nueva estructura del componente de nuevo turno del usuario

--- src/app/components/componente-nuevo-turno/componente-nuevo-turno.ts
+++ src/app/components/componente-nuevo-turno/componente-nuevo-turno.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-componente-nuevo-turno',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './componente-nuevo-turno.html',
   styleUrl: './componente-nuevo-turno.css',
 })
-export class ComponenteNuevoTurno {}
+export class ComponenteNuevoTurno { }
