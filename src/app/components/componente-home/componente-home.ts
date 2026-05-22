a1b06b16dd77b879091239162e4b3f445e69b7a4
piragna <gonzalovarelaalagna@gmail.com>
2026-05-21 21:00:43 -0300

    Nueva estructura del componente home del usuario.

--- src/app/components/componente-home/componente-home.ts
+++ src/app/components/componente-home/componente-home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-componente-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './componente-home.html',
   styleUrl: './componente-home.css',
 })
-export class ComponenteHome {}
+export class ComponenteHome { }
