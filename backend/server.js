52d13cb4ce1575997ab6ba8d67b1d02bf7551429
piragna <gonzalovarelaalagna@gmail.com>
2026-05-23 19:27:18 -0300

    Agregando endpoints para insertar usuario, obtener usuarios y logueo de usuario.

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-import express from 'epxress';
+import express from 'express';
 import mysql from 'mysql2';
 import cors from 'cors';
 
@@ -10,7 +10,7 @@ const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'turnera_db'
+    database: 'turnera'
 });
 
 db.connect(err => {
@@ -18,3 +18,36 @@ db.connect(err => {
     console.log('Conectado a MYSQL');
 });
 
+app.post('/usuarios', (req, res) => {
+    const { nombre, email, telefono, clave, rol, superadmin } = req.body;
+    db.query('insert into usuarios (nombre,email,telefono,clave,rol,superadmin) values (?,?,?,?,?,?) ',
+        [nombre, email, telefono, clave, rol, superadmin], (err, result) => {
+            if (err) return res.json(err);
+            res.json(result);
+        });
+})
+
+app.get("/usuarios", (req, res) => {
+    db.query("SELECT * FROM usuarios", (err, result) => {
+        if (err) res.status(500).send(err);
+        else res.json(result);
+    })
+});
+
+app.post("/login", (req, res) => {
+    const { email, clave } = req.body;
+    db.query("select * from usuarios where email = ? and clave = ?", [email, clave],
+        (err, result) => {
+            if (err) res.status(500).send(err);
+            if (result.length === 0) {
+                return res.status(401).json({ mensaje: "Credenciales invalidas" });
+            }
+            res.json({ mensaje: "Login exitoso", usuario: result[0] });
+        }
+    )
+});
+
+app.listen(3000, () => {
+    console.log("Servidor en http://localhost:3000");
+
+});
