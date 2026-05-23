028e36226876fc1e897f14d5c58ce05bcd73e493
piragna <gonzalovarelaalagna@gmail.com>
2026-05-23 19:26:27 -0300

    Componente de clientes del administrador

--- src/app/components/componente-clientes-administrador/componente-clientes-administrador.ts
+++ src/app/components/componente-clientes-administrador/componente-clientes-administrador.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Usuario } from '../../usuario/interface/usuario.interface';
+import { UsuarioService } from '../../usuario/services/usuario-service';
 
 @Component({
   selector: 'app-componente-clientes-administrador',
@@ -7,4 +9,19 @@ import { RouterLink } from "@angular/router";
   templateUrl: './componente-clientes-administrador.html',
   styleUrl: './componente-clientes-administrador.css',
 })
-export class ComponenteClientesAdministrador { }
+export class ComponenteClientesAdministrador implements OnInit {
+  us: UsuarioService = inject(UsuarioService);
+  usuarios: Usuario[] = [];
+  constructor() {}
+  ngOnInit(): void {
+    this.us.getUsuarios().subscribe({
+      next: (usuarios) => {
+        this.usuarios = usuarios;
+        console.log('usuarios:', this.usuarios);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+}
