afd3836df411c596d7385ec6f410fa1d11fad9a3
piragna <gonzalovarelaalagna@gmail.com>
2026-05-23 19:26:08 -0300

    Formulario de login

--- src/app/components/formulario-login/formulario-login.ts
+++ src/app/components/formulario-login/formulario-login.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { LoginService } from '../../login/services/login-service';
 
 @Component({
   selector: 'app-formulario-login',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './formulario-login.html',
   styleUrl: './formulario-login.css',
 })
-export class FormularioLogin { }
+export class FormularioLogin {
+  fb: FormBuilder = inject(FormBuilder);
+  formulario = this.fb.nonNullable.group({
+    email: ['', [Validators.required, Validators.email]],
+    clave: ['', [Validators.required]],
+  });
+  ls: LoginService = inject(LoginService);
+  r: Router = inject(Router);
+  iniciarSesion() {
+    if (this.formulario.invalid) return;
+    const { email, clave } = this.formulario.value;
+    this.ls.login(email, clave).subscribe({
+      next: (val) => {
+        console.log('usuario logueado:', val);
+        localStorage.setItem('usuario', JSON.stringify(val.usuario));
+        this.r.navigateByUrl('/home');
+      },
+      error: (e) => {
+        console.log(e);
+      },
+    });
+  }
+}
