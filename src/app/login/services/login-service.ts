fb8bfaa33aa4e437d8c33ec969543b2d6cdda1f5
piragna <gonzalovarelaalagna@gmail.com>
2026-05-23 19:23:56 -0300

    Creacion del loginservice

---
+++ src/app/login/services/login-service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginService {
+  private apiUrl: string = 'http://localhost:3000/login';
+  constructor(private http: HttpClient) {}
+  login(email: string | undefined, clave: string | undefined): Observable<any> {
+    return this.http.post(this.apiUrl, { email, clave });
+  }
+}
