a56a756b3e64035162ec6d8cda7c9a25d7bec418
piragna <gonzalovarelaalagna@gmail.com>
2026-05-24 13:42:06 -0300

    Implementacion del componente de alta de servicios

--- src/app/components/componente-alta-servicio/componente-alta-servicio.ts
+++ src/app/components/componente-alta-servicio/componente-alta-servicio.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { ServicioService } from '../../servicio/services/servicio-service';
 
 @Component({
   selector: 'app-componente-alta-servicio',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './componente-alta-servicio.html',
   styleUrl: './componente-alta-servicio.css',
 })
-export class ComponenteAltaServicio { }
+export class ComponenteAltaServicio {
+  fb: FormBuilder = inject(FormBuilder);
+  formulario = this.fb.nonNullable.group({
+    tipo: ['', [Validators.required]],
+    duracionMinutos: [0, [Validators.required, Validators.min(30)]],
+    precio: [0, [Validators.required, Validators.min(15000)]],
+  });
+  ss: ServicioService = inject(ServicioService);
+  generarServicio() {
+    if (this.formulario.invalid) return;
+    const s = this.formulario.getRawValue();
+    this.ss.postServicio(s).subscribe({
+      next: (s) => {
+        console.log('servicio posteado', s);
+        this.ss.setServiciosSignal(s);
+      },
+      error: (e) => {
+        console.log(e);
+      },
+    });
+  }
+}
