42dca8c95d715025e14ce655d50fc1d65c8b935e
piragna <gonzalovarelaalagna@gmail.com>
2026-05-24 13:38:32 -0300

    Agregando signal al servicio del usuario y los metodos para obtenerla y modificarla.

--- src/app/usuario/services/usuario-service.ts
+++ src/app/usuario/services/usuario-service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, signal, Signal } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Usuario } from '../interface/usuario.interface';
 
@@ -8,7 +8,19 @@ import { Usuario } from '../interface/usuario.interface';
 })
 export class UsuarioService {
   url: string = 'http://localhost:3000/usuarios';
+  usuarios = signal<Usuario[]>([]);
   constructor(private http: HttpClient) {}
+
+  getUsuariosSignal() {
+    return this.usuarios;
+  }
+
+  setUserSignal(usuario: Usuario) {
+    this.usuarios.update((actuales) => {
+      if (actuales.find((u) => u.id === usuario.id)) return actuales;
+      return [...actuales, usuario];
+    });
+  }
 
   postUsuario(usuario: Usuario): Observable<Usuario> {
     return this.http.post<Usuario>(this.url, usuario);
