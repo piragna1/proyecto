2c9e0fd3a151da8f8c847183c41fe119fefde23f
piragna <gonzalovarelaalagna@gmail.com>
2026-05-24 13:55:37 -0300

    Implementacion del componente que lista los servicios para el administrador

--- src/app/components/componente-servicios-administrador/componente-servicios-administrador.ts
+++ src/app/components/componente-servicios-administrador/componente-servicios-administrador.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ServicioService } from '../../servicio/services/servicio-service';
 
 @Component({
   selector: 'app-componente-servicios-administrador',
@@ -7,4 +8,21 @@ import { RouterLink } from '@angular/router';
   templateUrl: './componente-servicios-administrador.html',
   styleUrl: './componente-servicios-administrador.css',
 })
-export class ComponenteServiciosAdministrador { }
+export class ComponenteServiciosAdministrador implements OnInit {
+  ss: ServicioService = inject(ServicioService);
+  servicios = this.ss.getServiciosSignal();
+  constructor() {}
+  ngOnInit(): void {
+    this.ss.getServicios().subscribe({
+      next: (servicios) => {
+        console.log(servicios);
+        servicios.forEach((s) => {
+          this.ss.setServiciosSignal(s);
+        });
+      },
+      error: (e) => {
+        console.log(e);
+      },
+    });
+  }
+}
