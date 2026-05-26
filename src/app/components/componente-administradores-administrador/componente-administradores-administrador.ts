58791e9e341ece6c91cf053698dd847e4b8187f1
piragna <gonzalovarelaalagna@gmail.com>
2026-05-26 14:50:30 -0300

    Implementacion del componente de administradores del super administrador.

--- src/app/components/componente-administradores-administrador/componente-administradores-administrador.ts
+++ src/app/components/componente-administradores-administrador/componente-administradores-administrador.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { UsuarioService } from '../../usuario/services/usuario-service';
 
 @Component({
   selector: 'app-componente-administradores-administrador',
@@ -7,4 +8,21 @@ import { RouterLink } from '@angular/router';
   templateUrl: './componente-administradores-administrador.html',
   styleUrl: './componente-administradores-administrador.css',
 })
-export class ComponenteAdministradoresAdministrador { }
+export class ComponenteAdministradoresAdministrador implements OnInit {
+  us: UsuarioService = inject(UsuarioService);
+  administradores = this.us.getAdministradoresSignal();
+  ngOnInit(): void {
+    this.us.getUsuarios().subscribe({
+      next: (u) => {
+        const administradores = u.filter((u) => u.rol === 'administrador');
+        console.log(administradores);
+        administradores.forEach(element => {
+          this.us.setAdministradoresSignal(element);
+        });
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    })
+  }
+}
