8a1a2a987143f70b15c9d4eab46490f23ced9e1e
piragna <gonzalovarelaalagna@gmail.com>
2026-05-26 14:56:07 -0300

    Usuario service: - implementacion de signal para los peluqueros y administradores - implementacion de getUsuarioById()

--- src/app/usuario/services/usuario-service.ts
+++ src/app/usuario/services/usuario-service.ts
@@ -9,24 +9,47 @@ import { Usuario } from '../interface/usuario.interface';
 export class UsuarioService {
   url: string = 'http://localhost:3000/usuarios';
   usuarios = signal<Usuario[]>([]);
-  constructor(private http: HttpClient) {}
+  peluqueros = signal<Usuario[]>([]);
+  administradores = signal<Usuario[]>([]);
+  constructor(private http: HttpClient) { };
 
   getUsuariosSignal() {
     return this.usuarios;
-  }
+  };
 
   setUserSignal(usuario: Usuario) {
     this.usuarios.update((actuales) => {
       if (actuales.find((u) => u.id === usuario.id)) return actuales;
       return [...actuales, usuario];
     });
+  };
+
+  getPeluquerosSignal() { return this.peluqueros };
+  setPeluquerosSignal(peluquero: Usuario) {
+    this.peluqueros.update((actuales) => {
+      if (actuales.find((u) => u.id === peluquero.id)) return actuales;
+      return [...actuales, peluquero];
+    });
   }
+  getAdministradoresSignal() { return this.administradores; };
+  setAdministradoresSignal(administrador: Usuario) {
+    this.administradores.update((actuales) => {
+      if (actuales.find((a) => a.id === administrador.id)) return actuales
+      return [...actuales, administrador];
+    })
+  }
+
 
   postUsuario(usuario: Usuario): Observable<Usuario> {
     return this.http.post<Usuario>(this.url, usuario);
-  }
+  };
 
   getUsuarios(): Observable<Usuario[]> {
     return this.http.get<Usuario[]>(this.url);
-  }
-}
+  };
+
+  getUsuarioById(id: string): Observable<Usuario> {
+    return this.http.get<Usuario>(this.url + '/' + id);
+  };
+
+};
