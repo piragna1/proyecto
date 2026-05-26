54275652775ea9aa603b0132b1ecf430a2818d7d
piragna <gonzalovarelaalagna@gmail.com>
2026-05-26 14:51:06 -0300

    Implementacion del componente de alta de administradores del super administrador.

--- src/app/components/componente-alta-administrador/componente-alta-administrador.ts
+++ src/app/components/componente-alta-administrador/componente-alta-administrador.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from "@angular/router";
+import { Usuario } from '../../usuario/interface/usuario.interface';
+import { UsuarioService } from '../../usuario/services/usuario-service';
 
 @Component({
   selector: 'app-componente-alta-administrador',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './componente-alta-administrador.html',
   styleUrl: './componente-alta-administrador.css',
 })
-export class ComponenteAltaAdministrador { }
+export class ComponenteAltaAdministrador {
+  fb: FormBuilder = inject(FormBuilder);
+  formulario = this.fb.nonNullable.group({
+    email: ['', [Validators.required, Validators.email]],
+    telefono: ['', [Validators.required, Validators.minLength(10)]],
+    clave: ['', [Validators.required, Validators.minLength(8)]]
+  });
+  us: UsuarioService = inject(UsuarioService);
+  generarAdministrador() {
+    if (this.formulario.invalid) return;
+    console.log(this.formulario.getRawValue());
+    const a: Usuario = {
+      nombre: 'administrador',
+      email: this.formulario.controls.email.value,
+      telefono: this.formulario.controls.telefono.value,
+      clave: this.formulario.controls.clave.value,
+      rol: 'administrador',
+      superadmin: false
+    };
+    this.us.postUsuario(a).subscribe({
+      next: (a) => {
+        console.log('admin generado:', a);
+        this.us.setAdministradoresSignal(a);
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    })
+  }
+}
