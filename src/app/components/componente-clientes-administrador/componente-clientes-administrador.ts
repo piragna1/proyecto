e1555dbe47695572815dcb9c47014feb411d7466
piragna <gonzalovarelaalagna@gmail.com>
2026-05-26 14:57:23 -0300

    Filtro de clientes.

--- src/app/components/componente-clientes-administrador/componente-clientes-administrador.ts
+++ src/app/components/componente-clientes-administrador/componente-clientes-administrador.ts
@@ -12,13 +12,15 @@ import { UsuarioService } from '../../usuario/services/usuario-service';
 export class ComponenteClientesAdministrador implements OnInit {
   us: UsuarioService = inject(UsuarioService);
   usuarios = this.us.getUsuariosSignal();
-  constructor() {}
+  constructor() { }
   ngOnInit(): void {
     this.us.getUsuarios().subscribe({
       next: (usuarios) => {
         console.log(usuarios);
         usuarios.forEach((u) => {
-          this.us.setUserSignal(u);
+          if (u.rol === 'cliente') {
+            this.us.setUserSignal(u);
+          }
         });
       },
       error: (e) => {
