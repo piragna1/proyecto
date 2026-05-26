c6089c646f17f0ce27935c926f010ec1d5375c99
piragna <gonzalovarelaalagna@gmail.com>
2026-05-26 14:54:38 -0300

    Implementacion del componente de listado de servicios del peluquero.

--- src/app/components/componente-servicios-peluquero/componente-servicios-peluquero.ts
+++ src/app/components/componente-servicios-peluquero/componente-servicios-peluquero.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { ServicioService } from '../../servicio/services/servicio-service';
+import { Servicio } from '../../servicio/interface/servicio.interface';
 
 @Component({
   selector: 'app-componente-servicios-peluquero',
@@ -7,4 +9,29 @@ import { RouterLink } from "@angular/router";
   templateUrl: './componente-servicios-peluquero.html',
   styleUrl: './componente-servicios-peluquero.css',
 })
-export class ComponenteServiciosPeluquero { }
+export class ComponenteServiciosPeluquero implements OnInit {
+  ss: ServicioService = inject(ServicioService);
+  servicios = this.ss.getServiciosSignal();
+  ngOnInit(): void {
+    this.ss.getServicios().subscribe({
+      next: (s: any[]) => {
+        console.log(s);
+        s.forEach(element => {
+          console.log('elemento', element);
+          const servicio: Servicio = {
+            id: element.id,
+            tipo: element.tipo,
+            duracionMinutos: element.duracion_minutos,
+            precio: element.precio
+          };
+          console.log('servicio construido:', servicio);
+
+          this.ss.setServiciosSignal(servicio)
+        });
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    })
+  }
+}
