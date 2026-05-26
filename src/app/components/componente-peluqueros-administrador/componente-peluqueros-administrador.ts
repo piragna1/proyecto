c0d39d24343904482ed7db4aed463a63dcfba81d
piragna <gonzalovarelaalagna@gmail.com>
2026-05-26 14:53:31 -0300

    Implementacion del componente del listado de peluqueros del administrador.

--- src/app/components/componente-peluqueros-administrador/componente-peluqueros-administrador.ts
+++ src/app/components/componente-peluqueros-administrador/componente-peluqueros-administrador.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { UsuarioService } from '../../usuario/services/usuario-service';
 
 @Component({
   selector: 'app-componente-peluqueros-administrador',
@@ -7,4 +8,20 @@ import { RouterLink } from "@angular/router";
   templateUrl: './componente-peluqueros-administrador.html',
   styleUrl: './componente-peluqueros-administrador.css',
 })
-export class ComponentePeluquerosAdministrador { }
+export class ComponentePeluquerosAdministrador implements OnInit {
+  us: UsuarioService = inject(UsuarioService);
+  peluqueros = this.us.getPeluquerosSignal();
+  ngOnInit(): void {
+    this.us.getUsuarios().subscribe({
+      next: (usuarios) => {
+        console.log(usuarios);
+        const peluqueros = usuarios.filter((u) => u.rol === 'peluquero');
+        console.log(peluqueros);
+        peluqueros.forEach(element => {
+          this.us.setPeluquerosSignal(element);
+        });
+      },
+      error: (err) => { console.log(err); }
+    })
+  }
+}
