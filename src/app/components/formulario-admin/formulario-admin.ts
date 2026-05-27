07f64226dc3a854b8846b27101091fa530576564
piragna <gonzalovarelaalagna@gmail.com>
2026-05-27 09:24:49 -0300

    Implementacion del componente formulario-admin para iniciar sesion como administrador.

--- src/app/components/formulario-admin/formulario-admin.ts
+++ src/app/components/formulario-admin/formulario-admin.ts
@@ -1,9 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../auth/services/auth-service';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { LoginService } from '../../login/services/login-service';
+import { Router, RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-formulario-admin',
-  imports: [],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './formulario-admin.html',
   styleUrl: './formulario-admin.css',
 })
-export class FormularioAdmin {}
+export class FormularioAdmin {
+  as: AuthService = inject(AuthService);
+  fb: FormBuilder = inject(FormBuilder);
+  ls: LoginService = inject(LoginService);
+  formulario = this.fb.nonNullable.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]]
+  });
+  r: Router = inject(Router);
+  onLogin() {
+    console.log('aaa');
+
+    if (this.formulario.invalid) return;
+    console.log('bbb');
+
+    const { email, password } = this.formulario.value;
+    this.ls.loginAdmin(email, password).subscribe({
+      next: (response) => {
+        console.log('Login exitoso:', response);
+        if (response.rol !== 'administrador') {
+          console.log('Usuario no es administrador:', response);
+          return;
+        };
+        localStorage.setItem('usuario', JSON.stringify(response));
+        this.as.logIn();
+        this.r.navigateByUrl('/home-admin');
+      },
+      error: (err) => {
+        console.log('Error en login:', err);
+      }
+    })
+  }
+}
