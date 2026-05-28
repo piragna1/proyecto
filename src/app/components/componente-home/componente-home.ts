68f264369a61471c308e1042bdcd277864f62d09
piragna <gonzalovarelaalagna@gmail.com>
2026-05-28 16:43:03 -0300

    Consumo de localstorage para el saludo al usuario.

--- src/app/components/componente-home/componente-home.ts
+++ src/app/components/componente-home/componente-home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from "@angular/router";
 
 @Component({
@@ -7,4 +7,15 @@ import { RouterLink } from "@angular/router";
   templateUrl: './componente-home.html',
   styleUrl: './componente-home.css',
 })
-export class ComponenteHome { }
+export class ComponenteHome implements OnInit {
+  usuario: string = 'Usuario';
+  ngOnInit(): void {
+
+    const usuarioData = localStorage.getItem('usuario');
+
+    if (!usuarioData) return;
+    const usuario = JSON.parse(usuarioData);
+
+    this.usuario = usuario.nombre;
+  }
+}
