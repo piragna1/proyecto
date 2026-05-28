c607af3dccaecc452cc66013fa8819c44f9ccdcd
piragna <gonzalovarelaalagna@gmail.com>
2026-05-28 16:39:33 -0300

    Enrutamiento hacia la pagina de servicios del administrador

--- src/app/components/componente-alta-servicio/componente-alta-servicio.ts
+++ src/app/components/componente-alta-servicio/componente-alta-servicio.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { ServicioService } from '../../servicio/services/servicio-service';
 
 @Component({
@@ -17,13 +17,15 @@ export class ComponenteAltaServicio {
     precio: [0, [Validators.required, Validators.min(15000)]],
   });
   ss: ServicioService = inject(ServicioService);
+  r: Router = inject(Router);
   generarServicio() {
     if (this.formulario.invalid) return;
     const s = this.formulario.getRawValue();
     this.ss.postServicio(s).subscribe({
       next: (s) => {
         console.log('servicio posteado', s);
-        this.ss.setServiciosSignal(s);
+        this.formulario.reset();
+        this.r.navigateByUrl('/servicios-admin');
       },
       error: (e) => {
         console.log(e);
