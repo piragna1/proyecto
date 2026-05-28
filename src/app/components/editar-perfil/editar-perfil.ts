678064f432b2c10b68b7f448b69fbc1983584897
piragna <gonzalovarelaalagna@gmail.com>
2026-05-28 16:53:14 -0300

    Creacion e implementacion del componente a traves del cual el usuario modifica datos de su perfil.

---
+++ src/app/components/editar-perfil/editar-perfil.ts
@@ -0,0 +1,65 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ActivatedRoute, RouterLink } from "@angular/router";
+import { UsuarioService } from '../../usuario/services/usuario-service';
+
+@Component({
+  selector: 'app-editar-perfil',
+  imports: [ReactiveFormsModule, RouterLink],
+  templateUrl: './editar-perfil.html',
+  styleUrl: './editar-perfil.css',
+})
+export class EditarPerfil implements OnInit {
+  fb: FormBuilder = inject(FormBuilder);
+  formulario = this.fb.nonNullable.group({
+    nombre: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    telefono: ['', [Validators.required, Validators.minLength(10)]],
+    clave: ['', []],
+    rol: ['cliente'],
+    superadmin: [false],
+  });
+  ar: ActivatedRoute = inject(ActivatedRoute);
+  id: string | null = null;
+  us: UsuarioService = inject(UsuarioService);
+  ngOnInit(): void {
+    this.ar.paramMap.subscribe({
+      next: (value) => {
+        console.log(value.get('id'));
+        this.id = value.get('id');
+        this.getUsuarioById(this.id);
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    })
+  }
+  actualizarUsuario() {
+    if (this.formulario.invalid) return;
+
+    this.us.putUsuario(this.formulario.getRawValue(), this.id).subscribe({
+      next: (u) => {
+        console.log('usuario actualizado:', u);
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
+  }
+  getUsuarioById(id: string | null) {
+    this.us.getUsuarioById(id).subscribe(
+      {
+        next: (u) => {
+          console.log('usuario::');
+          this.formulario.controls.nombre.setValue(u.nombre);
+          this.formulario.controls.email.setValue(u.email);
+          this.formulario.controls.telefono.setValue(u.telefono);
+          console.log(u);
+        },
+        error: (err) => {
+          console.log(err);
+        }
+      }
+    )
+  }
+}
