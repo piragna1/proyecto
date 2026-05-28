e053684e0f319aad17ef34455d5058389c3e6297
piragna <gonzalovarelaalagna@gmail.com>
2026-05-28 16:27:23 -0300

    Funciones - obtenerTurnoPorId - actualizarTurno

--- backend/controllers/turnosController.js
+++ backend/controllers/turnosController.js
@@ -7,6 +7,19 @@ export function obtenerTurnos(db) {
     };
 };
 
+export function obtenerTurnoPorId(db) {
+    return (req, res) => {
+        const { id } = req.params;
+        console.log(id)
+        db.query("select * from turnos where id = ?", [id],
+            (err, result) => {
+                if (err) res.status(500).send(err);
+                else res.json(result[0]);
+            }
+        );
+    };
+};
+
 export function insertarTurno(db) {
     return (req, res) => {
         const turno = req.body;
@@ -32,6 +45,24 @@ export function eliminarTurno(db) {
                 if (err) return res.json(err);
                 return res.json(result);
             }
-        )
-    }
-}
+        );
+    };
+};
+
+export function actualizarTurno(db) {
+    return (req, res) => {
+        const { usuario, servicio, fechaHoraInicio, fechaHoraFin } = req.body;
+        const { id } = req.params;
+        console.log(usuario);
+        console.log(servicio);
+        console.log(fechaHoraInicio);
+        console.log(fechaHoraFin);
+        db.query('update turnos set id_usuario = ?, id_servicio = ?, fecha_hora_inicio = ?, fecha_hora_fin = ? where id = ?',
+            [usuario.id, servicio.id, fechaHoraInicio, fechaHoraFin, id],
+            (err, result) => {
+                if (err) return res.json(err);
+                return res.json(result);
+            }
+        );
+    };
+};
