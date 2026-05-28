f3fb14bd773b0aef828ef88a16ca6cf7c6a305ee
piragna <gonzalovarelaalagna@gmail.com>
2026-05-28 16:31:37 -0300

    cambios:
    - metodo get para el enrutador de turnos
    - metodo put para el enrutador de turnos

--- backend/routes/turnos.js
+++ backend/routes/turnos.js
@@ -1,14 +1,18 @@
 import { Router } from 'express';
-import { eliminarTurno, insertarTurno, obtenerTurnos } from '../controllers/turnosController.js';
+import { actualizarTurno, eliminarTurno, insertarTurno, obtenerTurnoPorId, obtenerTurnos } from '../controllers/turnosController.js';
 
 export default function (db) {
     const router = Router();
 
     router.get("/", obtenerTurnos(db));
 
+    router.get("/:id", obtenerTurnoPorId(db));
+
     router.post('/', insertarTurno(db));
 
     router.delete('/:id', eliminarTurno(db));
+
+    router.put('/:id', actualizarTurno(db));
 
     return router;
 }
