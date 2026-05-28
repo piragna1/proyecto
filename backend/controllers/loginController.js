471c8cd2bad001a85c54084cb26c7d85b838bb16
piragna <gonzalovarelaalagna@gmail.com>
2026-05-28 16:26:17 -0300

    Utilizacion de bcrypt para la compracion de claves.

--- backend/controllers/loginController.js
+++ backend/controllers/loginController.js
@@ -1,13 +1,21 @@
+import bcrypt from 'bcrypt';
 export function loginUsuario(db) {
     return (req, res) => {
         const { email, clave } = req.body;
-        db.query("select * from usuarios where email = ? and clave = ?", [email, clave],
-            (err, result) => {
+        db.query("select * from usuarios where email = ?", [email],
+            async (err, result) => {
                 if (err) return res.status(500).send(err);
                 if (result.length === 0) {
                     return res.status(401).json({ mensaje: "Credenciales invalidas" });
                 }
-                res.json({ mensaje: "Login exitoso", usuario: result[0] });
+                console.log('result: ', result);
+
+                const coincide = await bcrypt.compare(
+                    clave,
+                    result[0].clave
+                );
+                if (coincide) { return res.json(result[0]); }
+                return res.status(401).json({ mensaje: "Credenciales invalidas" });
             }
         );
     };
@@ -15,15 +23,24 @@ export function loginUsuario(db) {
 
 export function loginAdmin(db) {
     return (req, res) => {
-        const { email, password } = req.body;
-        db.query("select * from usuarios where email = ? and clave = ?", [email, password],
-            (err, result) => {
-                console.log('err, result', err, result);
+        const { email, clave } = req.body;
+        console.log(req.body);
+        console.log('clave', clave);
+        db.query("select * from usuarios where email = ?", [email],
+            async (err, result) => {
                 if (err) return res.status(500).send(err);
                 if (result.length === 0) {
                     return res.status(401).json({ mensaje: "Credenciales invalidas" });
                 }
-                res.json(result[0]);
+                console.log('result:', result[0]);
+
+                const coincide = await bcrypt.compare(
+                    clave,
+                    result[0].clave
+                );
+                console.log('coincide:', coincide)
+                if (coincide) { return res.json(result[0]); }
+                return res.status(401).json({ mensaje: "Credenciales invalidas" });
             }
         );
     };
