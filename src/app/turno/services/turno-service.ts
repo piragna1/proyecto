7e02a5b811a94a8634de2887aa510986790f0a7b
piragna <gonzalovarelaalagna@gmail.com>
2026-05-29 12:16:39 -0300

    metodos: - limpiarTurnosSignal para vaciar la signal de turnos - getTurnoById() http - putTurno() http

--- src/app/turno/services/turno-service.ts
+++ src/app/turno/services/turno-service.ts
@@ -12,25 +12,34 @@ export class TurnoService {
   constructor(private http: HttpClient) { }
   getTurnosSignal() {
     return this.turnos;
-  }
+  };
   setTurnosSignal(turno: Turno) {
     this.turnos.update((actuales) => {
       if (actuales.find((t) => t.id === turno.id)) return actuales;
       return [...actuales, turno];
     });
-  }
-  removerTurno(id: string) {
+  };
+  removerTurno(id: string | null) {
     this.turnos.update((actuales) => {
       return actuales.filter((t) => t.id !== id);
     });
+  };
+  limpiarTurnosSignal() {
+    this.turnos.set([]);
   }
   postTurno(turno: Turno): Observable<Turno> {
     return this.http.post<Turno>(this.url, turno);
-  }
+  };
   getTurnos(): Observable<any[]> {
     return this.http.get<any[]>(this.url);
-  }
-  deleteTurno(id: string): Observable<Turno> {
+  };
+  getTurnoById(id: string | null): Observable<Turno> {
+    return this.http.get<Turno>(this.url + '/' + id);
+  };
+  putTurno(t: Turno, id: string | null): Observable<Turno> {
+    return this.http.put<Turno>(this.url + '/' + id, t);
+  };
+  deleteTurno(id: string | null): Observable<Turno> {
     return this.http.delete<Turno>(this.url + '/' + id);
-  }
-}
+  };
+};
