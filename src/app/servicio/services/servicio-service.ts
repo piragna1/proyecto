12d9a111d6582aae929e63c1fc0429ae0ea5b341
piragna <gonzalovarelaalagna@gmail.com>
2026-05-29 12:12:52 -0300

    metodos: - removerServicio para remover un servicio de la signal, - limpiarServiciosSignal para vaciar la signal - putServicio() http - deleteServicio() http

--- src/app/servicio/services/servicio-service.ts
+++ src/app/servicio/services/servicio-service.ts
@@ -19,6 +19,15 @@ export class ServicioService {
       return [...actuales, servicio];
     });
   }
+  removerServicio(id: string | null) {
+    this.servicios.update((actuales) => {
+      return actuales.filter((t) => t.id !== id);
+    });
+  };
+  limpiarServiciosSignal() {
+    this.servicios.set([]);
+  }
+
   postServicio(servicio: Servicio): Observable<Servicio> {
     return this.http.post<Servicio>(this.url, servicio);
   }
@@ -27,7 +36,15 @@ export class ServicioService {
     return this.http.get<Servicio[]>(this.url);
   }
 
-  getServicioById(id: string): Observable<Servicio> {
+  getServicioById(id: string | null): Observable<Servicio> {
     return this.http.get<Servicio>(this.url + '/' + id);
+  }
+
+  putServicio(s: Servicio, id: string | null): Observable<Servicio> {
+    return this.http.put<Servicio>(this.url + '/' + id, s);
+  }
+
+  deleteServicio(id: string | undefined): Observable<Servicio> {
+    return this.http.delete<Servicio>(this.url + '/' + id);
   }
 }
