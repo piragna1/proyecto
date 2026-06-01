baff72effa10b82263aeee85bc24fc12c65a0d2b
gvalagna <gonzalovarelaalagna@gmail.com>
2026-06-01 13:00:19 -0300

    Utilizacion de variables de entorno.

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+//server.js
 import express from 'express';
 import mysql from 'mysql2';
 import cors from 'cors';
@@ -5,16 +6,18 @@ import loginRoutes from './routes/login.js'
 import serviciosRoutes from './routes/servicios.js'
 import turnosRoutes from './routes/turnos.js'
 import usuariosRoutes from './routes/usuarios.js'
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 const db = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'turnera'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 app.use('/login', loginRoutes(db));
