6af47aa5fbaf8e7fc0f978a367407aa5152cb5ef
gvalagna <gonzalovarelaalagna@gmail.com>
2026-06-02 16:39:31 -0300

    Agregando decodificacion del token al authGuard para el logeo de clientes.

--- src/app/guards/authGuard.ts
+++ src/app/guards/authGuard.ts
@@ -1,24 +1,39 @@
 import { inject } from "@angular/core";
 import { AuthService } from "../auth/services/auth-service";
 import { Router } from "@angular/router";
+import { jwtDecode } from "jwt-decode";
 
 export function authGuard() {
     console.log('authGuard ejecutado');
     const authService = inject(AuthService);
     const router: Router = inject(Router);
-    if (authService.estoyLogeado || localStorage.getItem('usuario')) {
-        const usuarioLogueado = JSON.parse(localStorage.getItem('usuario') || '{}');
-        if (usuarioLogueado.rol === 'cliente') {
-            return true;
+
+    const tokenRaw = localStorage.getItem('token');
+    const token = tokenRaw ? JSON.parse(tokenRaw) : null;
+    console.log('token!', token)
+    
+    if (!token) return false;
+    
+    try {
+        const payload:any = jwtDecode(token);
+        console.log('payload',payload);
+        if (authService.estoyLogeado && payload) {
+            if (payload.rol === 'cliente') {
+                return true;
+            }
+            if (payload.rol === 'administrador') {
+                router.navigateByUrl('/home-admin');
+                return false;
+            }
+            if (payload.rol === 'peluquero') {
+                router.navigateByUrl('/home-peluquero');
+                return false;
+            }
         }
-        if (usuarioLogueado.rol === 'administrador') {
-            router.navigateByUrl('/home-admin');
-            return false;
-        }
-        if (usuarioLogueado.rol === 'peluquero') {
-            router.navigateByUrl('/home-peluquero');
-            return false;
-        }
+    } catch (error) {
+        console.log('Error al decodificarl el token', error);
+        
     }
+
     return false;
 }
