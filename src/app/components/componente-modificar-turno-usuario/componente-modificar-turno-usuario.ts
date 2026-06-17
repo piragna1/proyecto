3eb8065439437fda26a9c611331295d61a44a560
gvalagna <gonzalovarelaalagna@gmail.com>
2026-06-17 17:47:11 -0300

    Inyeccion de authService y usuarioService

--- src/app/components/componente-modificar-turno-usuario/componente-modificar-turno-usuario.ts
+++ src/app/components/componente-modificar-turno-usuario/componente-modificar-turno-usuario.ts
@@ -3,12 +3,13 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute, Router, RouterLink } from "@angular/router";
 import { ServicioService } from '../../servicio/services/servicio-service';
 import { Turno } from '../../turno/interface/turno.interface';
-import { Usuario } from '../../usuario/interface/usuario.interface';
 import { Servicio } from '../../servicio/interface/servicio.interface';
 import { TurnoService } from '../../turno/services/turno-service';
 import { formatearServicio } from '../../servicio/utils/utils';
 import { formatearFechaSQL } from '../../shared/utils/dateHelpers';
 import { ToastService } from '../../shared/services/toast-service';
+import { AuthService } from '../../auth/services/auth-service';
+import { UsuarioService } from '../../usuario/services/usuario-service';
 
 @Component({
   selector: 'app-componente-modificar-turno-usuario',
@@ -29,6 +30,8 @@ export class ComponenteModificarTurnoUsuario implements OnInit {
   id: string | null = null;
   r: Router = inject(Router);
   toastService: ToastService = inject(ToastService);
+  as:AuthService= inject(AuthService);
+  us:UsuarioService=inject(UsuarioService);
   ngOnInit(): void {
 
     this.ss.getServicios().subscribe({
@@ -54,35 +57,41 @@ export class ComponenteModificarTurnoUsuario implements OnInit {
   }
   modificarTurno() {
     if (this.formulario.invalid) return;
-    const usuarioData = localStorage.getItem('usuario');
-    if (!usuarioData) return;
-    const u: Usuario = JSON.parse(usuarioData);
-    const servicio: Servicio = this.formulario.controls.servicio.value!;
-    const fechaHoraInicioRaw = this.formulario.controls.fechaHoraInicio.value;
-    const fechaHoraInicioDate = new Date(fechaHoraInicioRaw);
-    const fechaHoraFinDate = new Date(
-      fechaHoraInicioDate.getTime() + servicio.duracionMinutos * 60000,
-    );
-    const inicio = formatearFechaSQL(fechaHoraInicioDate);
-    const fin = formatearFechaSQL(fechaHoraFinDate);
-    const t: Turno = {
-      fechaHoraInicio: inicio,
-      fechaHoraFin: fin,
-      usuario: u,
-      servicio
-    };
-    console.log(t);
-    this.ts.putTurno(t, this.id).subscribe({
-      next: (value) => {
-        console.log('turno puteado', value);
-        this.ts.limpiarTurnosSignal();
-        this.r.navigateByUrl('/mis-turnos');
-        this.toastService.mostrarMensaje('Turno modificado correctamente.');
+    const payload = this.as.obtenerPayload();
+    if (!payload.id) return;
+    this.us.getUsuarioById(payload.id).subscribe({
+      next:(value)=>{
+        const servicio: Servicio = this.formulario.controls.servicio.value!;
+        const fechaHoraInicioRaw = this.formulario.controls.fechaHoraInicio.value;
+        const fechaHoraInicioDate = new Date(fechaHoraInicioRaw);
+        const fechaHoraFinDate = new Date(
+          fechaHoraInicioDate.getTime() + servicio.duracionMinutos * 60000,
+        );
+        const inicio = formatearFechaSQL(fechaHoraInicioDate);
+        const fin = formatearFechaSQL(fechaHoraFinDate);
+        const t: Turno = {
+          fechaHoraInicio: inicio,
+          fechaHoraFin: fin,
+          usuario: value,
+          servicio
+        };
+        this.ts.putTurno(t, this.id).subscribe({
+          next: (value) => {
+            console.log('turno puteado', value);
+            this.ts.limpiarTurnosSignal();
+            this.r.navigateByUrl('/mis-turnos');
+            this.toastService.mostrarMensaje('Turno modificado correctamente.');
+          },
+          error: (err) => {
+            console.log(err);
+          }
+        });
       },
-      error: (err) => {
+      error:(err)=>{
         console.log(err);
       }
     })
+   
   };
 
   getTurnoById(id: string | null) {
@@ -91,7 +100,8 @@ export class ComponenteModificarTurnoUsuario implements OnInit {
         console.log('value', value);
         this.ss.getServicioById(value.id_servicio).subscribe({
           next: (serv) => {
-
+            console.log(serv);
+            
             const fecha = new Date(value.fecha_hora_inicio);
             const fechaLocal = new Date(
               fecha.getTime() - fecha.getTimezoneOffset() * 60000
@@ -100,6 +110,7 @@ export class ComponenteModificarTurnoUsuario implements OnInit {
               .slice(0, 16);
 
             this.formulario.controls.fechaHoraInicio.setValue(fechaLocal);
+            this.formulario.controls.servicio.setValue(serv);
           }, error: (err) => {
             console.log(err);
           }
