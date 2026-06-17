5cf119f6f5a2e23a28ec0d618f87bb8ec5f730bb
gvalagna <gonzalovarelaalagna@gmail.com>
2026-06-17 16:26:13 -0300

    Agrego interceptor a la configuracion del proyecto

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,15 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { TokenInterceptor } from './interceptors/token.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes), provideHttpClient()],
+  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes), 
+    provideHttpClient(
+      withInterceptors([
+        (req, next) => new TokenInterceptor().intercept(req, { handle: next })
+      ])
+    )
+  ],
 };
