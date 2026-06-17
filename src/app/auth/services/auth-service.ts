fdf611748597f2cf5d54e4bfa0befaa3ab954581
gvalagna <gonzalovarelaalagna@gmail.com>
2026-06-17 16:27:07 -0300

    Metodos - obtenerPayload - obtenerRolUsuario - esTokenValido - esSuperadmin Que funcionan sobre el token jwt

--- src/app/auth/services/auth-service.ts
+++ src/app/auth/services/auth-service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-
+import { jwtDecode } from 'jwt-decode';
 @Injectable({
   providedIn: 'root',
 })
@@ -12,4 +12,34 @@ export class AuthService {
   logOut() {
     this.estoyLogeado = false;
   }
+  
+    obtenerPayload(){
+      const token = localStorage.getItem('token');
+      if(!token)return null;
+      try {
+        return jwtDecode<any>(token);
+      } catch (error) {
+        return null;
+      }
+    };
+  
+    obtenerRolUsuario(){
+      const payload=this.obtenerPayload();
+      return payload?.rol || null;
+    };
+  
+    esTokenValido(){
+      const payload = this.obtenerPayload();
+
+      console.log('payload', payload);
+      
+
+      if (!payload?.exp) return false;
+      return Date.now() < payload.exp * 1000;
+    };
+
+    esSuperAdmin(){
+      const payload = this.obtenerPayload();
+      return payload?.superadmin === 1;
+    };
 }
