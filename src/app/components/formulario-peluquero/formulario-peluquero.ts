4eef5f002bf8b6353d38f6111839721d836d5f7d
gvalagna <gonzalovarelaalagna@gmail.com>
2026-06-18 11:48:21 -0300

    Remuevo import no utilizado

--- src/app/components/formulario-peluquero/formulario-peluquero.ts
+++ src/app/components/formulario-peluquero/formulario-peluquero.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Router, RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 import { AuthService } from '../../auth/services/auth-service';
 import { LoginService } from '../../login/services/login-service';
 
