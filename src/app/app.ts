87f3aa267caad26804788b0321cd09c8b73fb62d
gvalagna <gonzalovarelaalagna@gmail.com>
2026-06-18 11:47:29 -0300

    Agrego el footer al componente principal

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Toast } from "./shared/components/toast/toast";
+import { Footer } from "./shared/components/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Toast],
+  imports: [RouterOutlet, Toast, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
