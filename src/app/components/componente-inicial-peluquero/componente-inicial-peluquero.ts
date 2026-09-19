9fed22386ce75839cd9641744be44c66411f6f2f
gvalagna <gonzalovarelaalagna@gmail.com>
2026-09-19 20:12:57 -0300

    feat: botones de nuevo turno y perfil en el home del peluquero

--- src/app/components/componente-inicial-peluquero/componente-inicial-peluquero.ts
+++ src/app/components/componente-inicial-peluquero/componente-inicial-peluquero.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../auth/services/auth-service';
 
 @Component({
   selector: 'app-componente-inicial-peluquero',
@@ -7,4 +8,12 @@ import { RouterLink } from "@angular/router";
   templateUrl: './componente-inicial-peluquero.html',
   styleUrl: './componente-inicial-peluquero.css',
 })
-export class ComponenteInicialPeluquero { }
+export class ComponenteInicialPeluquero {
+  r: Router = inject(Router);
+  as: AuthService = inject(AuthService);
+  irAPerfil() {
+    const payload = this.as.obtenerPayload();
+    if (!payload.id) return;
+    this.r.navigateByUrl(`/editar-perfil-peluquero/${payload.id}`);
+  }
+}
